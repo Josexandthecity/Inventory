d1a1e4ab13aa7cd8d0c12996e7fee8409c983aa7
Jose Forero <joseforero@thepublicgroup.com>
2025-05-04 20:00:15 -0500

    Add project files.

---
+++ Inventory/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Inventory.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Inventory.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
